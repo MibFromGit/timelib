285c6d3f4d964a110091277dd63bddc7df65043f
MibFromGit <pascalboerger@outlook.com>
2022-05-17 08:18:16 +0200

    Schaltjahr umbau + Parameteränderungen

--- main.c
+++ main.c
@@ -1,90 +1,94 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int day_of_the_Year(int T, int M, int J)
+int day_of_the_Year(int days, int month, int year)
 {
     int tempdays = 0;
     int tempmonth;
-    for (int i = 0; i < M - 1; i++)
+    for (int i = 0; i < month - 1; i++)
     {
         tempmonth = i + 1;
-        tempdays += get_days_for_months(tempmonth, J);
+        tempdays += get_days_for_months(tempmonth, year);
     }
-    return tempdays += T;
+    return tempdays += days;
 }
 
-int is_leapyear(int j)
+int is_leapyear(int year)
 {
-    if(j < 1581 && j > 2400)
-    {
+    if(year < 1582)
         return -1;
-    }
-    if(j % 4 == 0) // prüfung ob das Jahr ein Schaltjahr ist
+
+    if(year % 4 == 0)
     {
-        return 1;
-    }
-    else if(j %400 == 0)// prüfung ob das Jahr ein Schaltjahr ist
-    {
-        return 1;
+        if(year % 100 == 0)
+        {
+            if(year % 400 == 0)
+            {
+                return 1; //Ist ein Schaltjahr
+            }
+        }else
+        {
+            return 1;
+        }
     }
     return 0;
 }
 
-int get_days_for_months(int M, int J)
+
+int get_days_for_months(int month, int year)
 {
     int tage_pro_monat[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
 
-    if(is_leapyear(J))
+    if (is_leapyear(year))
     {
         tage_pro_monat[1] = 29;
     }
-    if (M < 13 && M > 0)
+    if (month < 13 && month > 0)
     {
-        return tage_pro_monat[M - 1];
+        return tage_pro_monat[month - 1];
     }
     return -1;
 }
 
-int exists_date(int T, int M, int J)
+int exists_date(int days, int month, int year)
 {
-    if(J > 1582 && J < 2400)
+    if (year > 1582)
     {
-        if(M > 0 && M < 13)
+        if (month > 0 && month < 13)
         {
-            if (T > 0 && T <= get_days_for_months(M,J))
+            if (days > 0 && days <= get_days_for_months(month,year))
             {
                 return 1;
             }
         }
-        return 0;
     }
     return 0;
 }
 
 int main()
 {
-    int Jahr;
-    int Monat;
-    int Tage;
+    int year;
+    int month;
+    int days;
     int Tag_des_Jahres = 0;
 
     do
     {
         printf("Geben sie ein Jahr ein: ");
-        scanf("%i", &Jahr);
+        scanf("%i", &year);
 
         printf("\nGeben sie einen Monat ein: ");
-        scanf("%i", &Monat);
+        scanf("%i", &month);
 
         printf("\nGeben sie einen Tag ein: ");
-        scanf("%i", &Tage);
+        scanf("%i", &days);
 
-        if(!exists_date(Tage, Monat, Jahr))
+        if(!exists_date(days, month, year))
         {
             printf("\nFalsche eingabe!\n versuchen sie es ernuert.\n");
         }
 
-    }while(!exists_date(Tage, Monat, Jahr));
+    }while(!exists_date(days, month, year));
 
-    printf("\nDer Tag der Jahres %i ist %i", Jahr, day_of_the_Year(Tage,Monat,Jahr));
+    printf("\nDer Tag der Jahres %i ist %i", year, day_of_the_Year(days,month,year));
 }
